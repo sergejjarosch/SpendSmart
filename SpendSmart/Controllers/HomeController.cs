77058df801aab3bd1cf4f5bff95bbf76f88bdb2b
sergejjarosch <sergejjarosch@gmail.com>
2024-06-18 16:30:32 +0200

    Init Project

---
+++ SpendSmart/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SpendSmart.Models;
+
+namespace SpendSmart.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Expenses()
+    {
+        return View();
+    }
+    
+    public IActionResult CreateEditExpense()
+    {
+        return View();
+    }
+
+    public IActionResult CreateEditExpenseFor(Expense model)
+    {
+        return RedirectToAction("Expenses");
+    }
+    
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
